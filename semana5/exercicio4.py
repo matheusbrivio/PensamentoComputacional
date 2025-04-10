0017f2aa6473899fa6c037ac5477b6f0c1559fec
matheusbrivio <matheusbrivio22@gmail.com>
2025-04-10 16:10:04 -0300

    Create exercicio4.py

---
+++ semana5/exercicio4.py
@@ -0,0 +1,6 @@
+codigo = int(input("Digite o codigo:"))
+senha = input("Digite a senha:")
+if codigo == 1234 and senha == "chave":
+    print("Acesso ao cofre liberado!")
+else:
+    print("Acesso negado!")
