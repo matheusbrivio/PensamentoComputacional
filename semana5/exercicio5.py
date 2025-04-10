c2902e0d92e2c29e8816125fe9fbd0d85c22842c
matheusbrivio <matheusbrivio22@gmail.com>
2025-04-10 16:13:07 -0300

    Create exercicio5.py

---
+++ semana5/exercicio5.py
@@ -0,0 +1,5 @@
+diasemana = input("insira o dia da semana:").upper()
+if diasemana == "SABADO" or diasemana == "DOMINGO":
+    print("Hoje é dia de descanso!")
+else:
+    print("Vamos trabalhar ou estudar!")
