7f179a35c56e4d7e0f546351f5eddbdfe40f6871
matheusbrivio <matheusbrivio22@gmail.com>
2025-05-08 21:18:41 -0300

    Update exercicio1.py

--- semana10/exercicio1.py
+++ semana10/exercicio1.py
@@ -1,8 +1,4 @@
-temperaturas = [30, 22, 25, 28, 31, 27, 29]
-
-for i in range (7):
-    if temperaturas[i] < 25:
-        temperaturas[i] += 5
-        print(temperaturas[i])
-    print(temperaturas[i])  
+animais = ["cachorro", "gato", "papagaio", "tartaruga"]
+print(animais[0], animais[3])
+animais[1] = "coelho"
     
