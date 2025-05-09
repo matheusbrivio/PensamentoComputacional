f085e7d7630f59c4e0f9fa2898c363a4d0cca33d
matheusbrivio <matheusbrivio22@gmail.com>
2025-05-08 21:19:21 -0300

    Create exercicio2.py

---
+++ semana10/exercicio2.py
@@ -0,0 +1,8 @@
+temperaturas = [30, 22, 25, 28, 31, 27, 29]
+
+for i in range (7):
+    if temperaturas[i] < 25:
+        temperaturas[i] += 5
+        print(temperaturas[i])
+    print(temperaturas[i])  
+    
