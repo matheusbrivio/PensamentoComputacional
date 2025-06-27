86a8b41eb170988cdb09fbe89179a4e9159de511
matheusbrivio <matheusbrivio22@gmail.com>
2025-06-26 21:21:17 -0300

    Create exercicio3.py

---
+++ dinamicag2/exercicio3.py
@@ -0,0 +1,10 @@
+def main():
+    numero1 = int(input("Digite o numero"))
+    numero2 = int(input("Digite o numero"))
+    soma = somarnumeros(numero1, numero2)
+
+def somarnumeros(n1,n2):
+    soma = n1 + n2
+    print(soma)
+
+main()
